d426b5ee4d516dd500a477d00bed3bfdbf3a6443
DrZebra <89104158+DrZebraZ@users.noreply.github.com>
2022-06-29 22:21:00 -0300

    refatorando o repositório

--- gerenciadortcc-app/src/components/deslogar.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-function Deslogar(){
-    localStorage.clear()
-}
-export default Deslogar
--- gerenciadortcc-app/src/main/RequireAuth.js
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from "react"
-import { Navigate } from "react-router-dom"
-import { useUserContext } from "../context/UserLogedContext"
-
-function RequireAuth({children}){
-  const {isLogado} = useUserContext()
-  return isLogado? children : <Navigate to="/login"/>
-}
-export default RequireAuth
